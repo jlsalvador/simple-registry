2a696443492829df405fbe8a2e94a89f35888050
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2025-12-16 14:14:22 +0100

    replace cmd help tabs by spaces

--- internal/cmd/help.go
+++ internal/cmd/help.go
@@ -11,36 +11,36 @@ Copyright 2025 José Luis Salvador Rufo <salvador.joseluis@gmail.com>
 
 Usage:
   simple-registry -genhash
-      Generate a password hash and exit.
+    Generate a password hash and exit.
 
   simple-registry [options]
-      Start the registry server.
+    Start the registry server.
 
   simple-registry -version
-      Print the version and exit.
+    Print the version and exit.
 
 Options:
   Required:
-	-adminname string
-        Administrator username.
-	-adminpwd string
-		Administrator password
-		(use with care, ignored if -adminpwd-file is set).
+    -adminname string
+      Administrator username.
+    -adminpwd string
+      Administrator password
+      (use with care, ignored if -adminpwd-file is set).
     -adminpwd-file string
-        File containing the administrator password
-		(takes precedence over -adminpwd).
+      File containing the administrator password
+      (takes precedence over -adminpwd).
 
   Optional flags:
-	-addr string
-        Listening address (default "0.0.0.0:5000")
-	-datadir string
-        Data directory (default "./data")
-	-cert string
-        TLS certificate (enables HTTPS).
-	-key string
-        TLS key.
-	-rbacdir string
-        Directory containing RBAC YAML definitions.
+    -addr string
+      Listening address (default "0.0.0.0:5000")
+    -datadir string
+      Data directory (default "./data")
+    -cert string
+      TLS certificate (enables HTTPS).
+    -key string
+      TLS key.
+    -rbacdir string
+      Directory containing RBAC YAML definitions.
 
 Examples:
   simple-registry \
