8078b50ec6c61f963c9381dbd9ed369757a43e38
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2026-01-03 20:31:10 +0100

    fix boolean flags for cmd garbage_collect

--- internal/cmd/garbage_collect/flags.go
+++ internal/cmd/garbage_collect/flags.go
@@ -21,34 +21,32 @@ type Flags struct {
 	LastAccess     time.Duration
 }
 
+// getBool parses a string value as a boolean.
+// If the parsing fails, it returns false.
+func getBool(val string) bool {
+	if val, err := strconv.ParseBool(val); err != nil {
+		return false
+	} else {
+		return val
+	}
+}
+
 func parseFlags() (flags Flags, err error) {
 	flagSet := flag.NewFlagSet("", flag.ExitOnError)
 
 	flagSet.StringVar(&flags.DataDir, "datadir", common.GetEnv(cmd.ENV_PREFIX+"DATADIR", "./data"), "Data directory")
 	flagSet.Var(&flags.CfgDir, "cfgdir", "Directory with YAML configuration files\nCould be specified multiple times")
 
-	var deleteUntagged string
-	flagSet.StringVar(&deleteUntagged, "delete-untagged", common.GetEnv(cmd.ENV_PREFIX+"DELETE_UNTAGGED", "false"), "If set, the command will delete manifests that are not currently referenced by a tag.")
-	var dryRun string
-	flagSet.StringVar(&dryRun, "dryrun", common.GetEnv(cmd.ENV_PREFIX+"DRYRUN", "false"), "If set, the command will not actually remove any blobs.")
-	var lastAccess string
-	flagSet.StringVar(&lastAccess, "last-access", common.GetEnv(cmd.ENV_PREFIX+"LAST_ACCESS", "24h"), "The time since the last access to a file before it is considered garbage.\nFormat: 1h, 2m, 3s, etc. Default: 24h.")
+	flagSet.BoolVar(&flags.DeleteUntagged, "delete-untagged", getBool(common.GetEnv(cmd.ENV_PREFIX+"DELETE_UNTAGGED", "false")), "If set, the command will delete manifests that are not currently referenced by a tag.")
+	flagSet.BoolVar(&flags.DryRun, "dryrun", getBool(common.GetEnv(cmd.ENV_PREFIX+"DRYRUN", "false")), "If set, the command will not actually remove any blobs.")
+
+	lastAccess := flagSet.String("last-access", common.GetEnv(cmd.ENV_PREFIX+"LAST_ACCESS", "24h"), "The time since the last access to a file before it is considered garbage.\nFormat: 1h, 2m, 3s, etc. Default: 24h.")
 
 	if err = flagSet.Parse(os.Args[2:]); err != nil {
 		return
 	}
 
-	flags.DeleteUntagged, err = strconv.ParseBool(deleteUntagged)
-	if err != nil {
-		return
-	}
-
-	flags.DryRun, err = strconv.ParseBool(dryRun)
-	if err != nil {
-		return
-	}
-
-	flags.LastAccess, err = time.ParseDuration(lastAccess)
+	flags.LastAccess, err = time.ParseDuration(*lastAccess)
 	if err != nil {
 		return
 	}
