2a7c2192fb6344f4490e2e80deedbc3acbde70db
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2026-02-02 09:29:41 +0100

    pkg: fix regexp registry name proper escapes

--- pkg/registry/validator.go
+++ pkg/registry/validator.go
@@ -2,7 +2,7 @@ package registry
 
 import "regexp"
 
-const ExprName = "^[a-z0-9]+(?:(?:.|_|__|-+)[a-z0-9]+)*(?:/[a-z0-9]+(?:(?:.|_|__|-+)[a-z0-9]+)*)*$"
+const ExprName = "^[a-z0-9]+(?:(?:\\.|_|__|-+)[a-z0-9]+)*(?:\\/[a-z0-9]+(?:(?:\\.|_|__|-+)[a-z0-9]+)*)*$"
 const ExprTag = "^[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}$"
 const ExprDigest = "^[a-z0-9]+(?:[+._-][a-z0-9])?:[a-zA-Z0-9=_-]+$"
 const ExprUUID = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"
