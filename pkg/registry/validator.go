061788bb97d12fd49c6112a4088a5b53dc8da634
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2026-02-02 09:30:42 +0100

    pkg: fix regexp registry digest algo component

--- pkg/registry/validator.go
+++ pkg/registry/validator.go
@@ -4,7 +4,7 @@ import "regexp"
 
 const ExprName = "^[a-z0-9]+(?:(?:\\.|_|__|-+)[a-z0-9]+)*(?:\\/[a-z0-9]+(?:(?:\\.|_|__|-+)[a-z0-9]+)*)*$"
 const ExprTag = "^[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}$"
-const ExprDigest = "^[a-z0-9]+(?:[+._-][a-z0-9])?:[a-zA-Z0-9=_-]+$"
+const ExprDigest = "^[a-z0-9]+(?:[+._-][a-z0-9]+)?:[a-zA-Z0-9=_-]+$"
 const ExprUUID = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"
 
 var RegExprName = regexp.MustCompile(ExprName)
