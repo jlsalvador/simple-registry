04be708cb27f8c804bde6213de52dfc10c114505
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2026-02-21 20:04:15 +0100

    cmd: adapt generate_hash for testing

--- internal/cmd/generate_hash/generate_hash.go
+++ internal/cmd/generate_hash/generate_hash.go
@@ -1,7 +1,22 @@
+// Copyright 2025 José Luis Salvador Rufo <salvador.joseluis@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package generatehash
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	cliTerm "github.com/jlsalvador/simple-registry/pkg/cli/term"
@@ -12,18 +27,17 @@ import (
 const CmdName = "genhash"
 const CmdHelp = "Generate a hash for the given password and exit"
 
-func isPiped() bool {
-	return !cliTerm.IsTerminal(os.Stdin)
-}
+var IsTerminal = cliTerm.IsTerminal
+var ReadPassword = term.ReadPassword
 
 func CmdFn() error {
 	var pwd []byte
 	var err error
-	if isPiped() {
-		pwd, err = os.ReadFile(os.Stdin.Name())
+	if !IsTerminal(os.Stdin) {
+		pwd, err = io.ReadAll(os.Stdin)
 	} else {
 		fmt.Fprint(os.Stderr, "Enter password (no echo): ")
-		pwd, err = term.ReadPassword(int(os.Stdin.Fd()))
+		pwd, err = ReadPassword(int(os.Stdin.Fd()))
 		fmt.Fprintln(os.Stderr, "")
 	}
 	if err != nil {
